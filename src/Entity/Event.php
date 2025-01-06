32f253afdfce0dae25efae28d378a6d7c9aaf82c
adryyy10 <adriafigueresgarcia@gmail.com>
2025-01-06 08:56:55 +0000

    feat: create project + Event entity

---
+++ src/Entity/Event.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventRepository::class)]
+class Event
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $totalSeats = null;
+
+    #[ORM\Column]
+    private ?int $availableSeats = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getTotalSeats(): ?int
+    {
+        return $this->totalSeats;
+    }
+
+    public function setTotalSeats(int $totalSeats): static
+    {
+        $this->totalSeats = $totalSeats;
+
+        return $this;
+    }
+
+    public function getAvailableSeats(): ?int
+    {
+        return $this->availableSeats;
+    }
+
+    public function setAvailableSeats(int $availableSeats): static
+    {
+        $this->availableSeats = $availableSeats;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+        $this->availableSeats = $this->totalSeats;
+    }
+}
